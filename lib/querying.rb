70e8421b86716679ebf6410d997a41a8bba1254c
Kathy Hudson <khitx@icloud.com>
2018-05-02 17:13:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,21 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year
+    FROM books
+    WHERE series = 1
+    ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto 
+    FROM characters
+    HAVING MAX(motto)"
 end
 
-
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species)
+    FROM characters
+    GROUP BY species
+    HAVING MAX(COUNT(species))"
 end
 
 def select_name_and_series_subgenres_of_authors
