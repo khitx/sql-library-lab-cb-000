04a384fdb0485d4b4c5ba7eed5dbe85b654297c2
Kathy Hudson <khitx@icloud.com>
2018-05-02 17:20:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,12 +21,12 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
     FROM authors
-    INNER JOIN series ON authors.id = series.author_id 
+    INNER JOIN series ON authors.id = series.author_id
     INNER JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  
 end
 
 def select_character_names_and_number_of_books_they_are_in
