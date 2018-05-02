69c6653a9f89b3b8afbd59e48f44c2f2ea08a588
Kathy Hudson <khitx@icloud.com>
2018-05-02 17:56:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,9 +31,10 @@ def select_series_title_with_most_human_characters
   "SELECT series.title, characters.species, COUNT(characters.species)
     FROM series
     INNER JOIN characters ON series.id = characters.series_id
-    GROUP BY characters.series_id
+    WHERE characters.species = 'Human'
      "
 end
+# GROUP BY characters.series_id
 # WHERE characters.species = 'Human'
 #{}"SELECT series.title
 #  FROM series
