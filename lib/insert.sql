8c94b8016944d7268abd1a5a49aadc35318031f8
Kathy Hudson <khitx@icloud.com>
2018-05-02 16:43:28 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -23,9 +23,20 @@ INSERT INTO characters VALUES (6, "Character 6", "Motto 6", "cat", 2, 2);
 INSERT INTO characters VALUES (7, "Character 7", "Motto 7", "human", 2, 2);
 INSERT INTO characters VALUES (8, "Character 8", "Motto 8", "cat", 2, 2);
 
+INSERT INTO character_books VALUES (1, 1, 1);
+INSERT INTO character_books VALUES (2, 1, 2);
+INSERT INTO character_books VALUES (3, 1, 3);
+INSERT INTO character_books VALUES (4, 2, 1);
+INSERT INTO character_books VALUES (5, 2, 2);
+INSERT INTO character_books VALUES (6, 2, 3);
+INSERT INTO character_books VALUES (7, 3, 1);
+INSERT INTO character_books VALUES (8, 4, 3);
 
-CREATE TABLE character_books (
-  id INTEGER PRIMARY KEY,
-  character_id INTEGER,
-  book_id INTEGER
-);
+INSERT INTO character_books VALUES (9, 5, 4);
+INSERT INTO character_books VALUES (10, 5, 5);
+INSERT INTO character_books VALUES (11, 5, 6);
+INSERT INTO character_books VALUES (12, 6, 4);
+INSERT INTO character_books VALUES (13, 6, 5);
+INSERT INTO character_books VALUES (14, 6, 6);
+INSERT INTO character_books VALUES (15, 7, 2);
+INSERT INTO character_books VALUES (16, 8, 3);
