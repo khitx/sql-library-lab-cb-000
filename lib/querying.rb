6bf606f834f7086a05500d744f6edd18cf4276f9
Kathy Hudson <khitx@icloud.com>
2018-05-02 17:31:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,8 +8,8 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto
     FROM characters
-    GROUP BY motto
-    HAVING MAX(motto)"
+    ORDER BY motto DESC
+    LIMIT 1"
 end
 
 def select_value_and_count_of_most_prolific_species
