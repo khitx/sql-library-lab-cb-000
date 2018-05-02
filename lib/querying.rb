9b06d469445f55bd82210ae31d3969371e009f68
Kathy Hudson <khitx@icloud.com>
2018-05-02 17:20:00 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,7 +6,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto 
+  "SELECT name, motto
     FROM characters
     HAVING MAX(motto)"
 end
@@ -19,7 +19,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+    FROM authors
+    INNER JOIN series ON authors.id = series.author_id 
+    INNER JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
